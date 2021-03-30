cb1dad26fd54f82acf8f3d3188bd542009855679
devalc <deva4998@vandals.uidaho.edu>
2021-03-29 19:02:11 -0700

    bunch of swat related updates. Some still TBD

--- global.R
+++ global.R
@@ -1,6 +1,6 @@
 # Add a custom thumbnail with a description
 # 
-# Need to figure out how to add action button to this to navigate through tabs
+
 # @param image a object of class character
 # @param label a object of class character
 # @param content a object of class character
@@ -9,37 +9,46 @@
 #
 #
 
+# thumbnail_label1 <- function(image, label, content){
+#     div(class = "row", style="height:360px",
+#         div(class = "col-sm-14 col-md-12",
+#             div(class = "thumbnail",style="background-color:#17141D;height:60px",
+#                 img(src = image, alt = "...", width ="100%",
+#                     div(class = "caption",style="color:#ffffff;max-width: 100%", h3(label), p(content)
+#                     )))))
+# }
+
+
 thumbnail_label1 <- function(image, label, content){
-    div(class = "row",
+    div(class = "row", style="height:370px",
         div(class = "col-sm-14 col-md-12",
-            div(class = "thumbnail",style="height:360px;background-color:#17141D;",
-                img(src = image, alt = "...",
-                    div(class = "caption",style="color:#ffffff;", h3(label), p(content)
+            div(class = "thumbnail",style="height:50px",
+                img(src = image, alt = "...", width ="100%",
+                    div(class = "caption",style="max-width: 100%;text-align: centre;", h3(label), p(content)
                     )))))
 }
 
-
 ### customize the html bakground image function to blur the image
 
-setBackgroundImage1 <- function(src = NULL, shinydashboard = FALSE) {
-    if (isTRUE(shinydashboard)) {
-        el <- ".content-wrapper"
-    } else {
-        el <- "body"
-    }
-    css <- paste0(
-        el, " {background: url(", src, ") no-repeat center center fixed;
-           -webkit-background-size: cover;
-           -moz-background-size: cover;
-           -o-background-size: cover;
-           background-size: cover;
-        backdrop-filter: blur(50px);}"
-    )
-    tags$head(tags$style(HTML(css)))
-}
+# setBackgroundImage1 <- function(src = NULL, shinydashboard = FALSE) {
+#     if (isTRUE(shinydashboard)) {
+#         el <- ".content-wrapper"
+#     } else {
+#         el <- "body"
+#     }
+#     css <- paste0(
+#         el, " {background: url(", src, ") no-repeat center center fixed;
+#            -webkit-background-size: cover;
+#            -moz-background-size: cover;
+#            -o-background-size: cover;
+#            background-size: cover;
+#         backdrop-filter: blur(50px);}"
+#     )
+#     tags$head(tags$style(HTML(css)))
+# }
 
 disconnected <- tagList(
     h1("Whoah there..."),
-    p("Something went terribly wrong!"),
+    p("Session timed out or something went terribly wrong! Hit Refresh..."),
     reload_button("REFRESH", class = "warning")
 )
